eb5972661d8eac3cf59b728c9b1cdbe5bfb46677
Arya <mirpourarya@berkeley.edu>
2022-01-04 23:39:54 -0800

    ye

---
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "New Website for Sharing Data",
+        "excerpt":"I uploaded tables and figures in the “General Bechmark Resuts” Most of the site is still under development. – Arya ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/web_bench/jekyll/update/2022/01/02/website_update.html"
+      }]
